1b1d990e49fccf5ae2d59dfb8f3e46f74f416fd5
Jakub Zalas <jakub@zalas.pl>
2013-11-30 21:30:31 +0100

    Set up automatic project installation.

--- tools/chef/site-cookbooks/symfony/recipes/default.rb
+++ tools/chef/site-cookbooks/symfony/recipes/default.rb
@@ -1,4 +1,24 @@
 
+execute "composer create-project" do
+  command <<-EOF
+    rm -rf /tmp/symfony.dev
+    cd /tmp
+    composer create-project --no-install symfony/framework-standard-edition /tmp/symfony.dev
+    mv /tmp/symfony.dev/* #{node[:symfony][:project_path]}
+    mv /tmp/symfony.dev/.travis.yml #{node[:symfony][:project_path]}/
+    cat /tmp/symfony.dev/.gitignore >> #{node[:symfony][:project_path]}/.gitignore
+    rm -rf /tmp/symfony.dev
+  EOF
+  action :run
+  creates "#{node[:symfony][:project_path]}/composer.json"
+end
+
+execute "composer install" do
+  command "composer install"
+  action :run
+  cwd node[:symfony][:project_path]
+end
+
 web_app node[:symfony][:domain] do
   server_name node[:symfony][:domain]
   server_aliases node[:symfony][:aliases]
@@ -8,4 +28,3 @@ web_app node[:symfony][:domain] do
   cookbook "symfony"
   template "symfony.conf.erb"
 end
-
